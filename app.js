e7760c147822acbb2b0c91043b4dcb67201e1fea
djole <djordjino@gmail.com>
2021-12-13 19:10:31 +0100

    github commit

--- app.js
+++ app.js
@@ -45,19 +45,129 @@
 //   }
 // }
 
+const words = [
+  "Hello Stranger.",
+  "Welcome to the random access memory.",
+  "Think of me now, as the input.",
+  "You are the medium. ",
+  " We are the network.",
+  "But the reality is our output.",
+  "Sounds cringy?",
+  "Yep. You guessed right. ",
+  "Do make the next choice, with care.",
+];
+
+function fnBrowserDetect() {
+  let userAgent = navigator.userAgent;
+  let browserName;
+
+  if (userAgent.match(/chrome|chromium|crios/i)) {
+    browserName = "chrome";
+  } else if (userAgent.match(/firefox|fxios/i)) {
+    browserName = "firefox";
+  } else if (userAgent.match(/safari/i)) {
+    browserName = "safari";
+  } else if (userAgent.match(/opr\//i)) {
+    browserName = "opera";
+  } else if (userAgent.match(/edg/i)) {
+    browserName = "edge";
+  } else {
+    browserName = "No browser detection";
+  }
+
+  return browserName;
+}
+
+fnBrowserDetect();
+
 const title = document.getElementById("titleID");
 const subtitle1 = document.getElementsByClassName("subtitle1");
 const subtitle2 = document.getElementsByClassName("subtitle2");
 const redBtn = document.getElementById("red-pill");
 const blueBtn = document.getElementById("blue-pill");
+const nextBtn = document.getElementById("next-btn-wrapper");
 
 const doorOne = document.getElementById("doorOne");
 const doorTwo = document.getElementById("doorTwo");
 const doorThree = document.getElementById("doorThree");
 
+//browser compensation
+const leftPercentage =
+  fnBrowserDetect() === "firefox"
+    ? (doorTwo.style.height = "133vh")
+    : (doorTwo.style.height = "123vh");
+
+const rightPercange =
+  fnBrowserDetect() === "firefox"
+    ? (doorThree.style.height = "133vh")
+    : (doorThree.style.height = "123vh");
+
 //Animations
-// gsap.to(doorOne, { duration: 2, y: "-85%", delay: 1 });
-// gsap.to(doorTwo, { duration: 2, x: "-55%", delay: 1 });
-// gsap.to(doorThree, { duration: 2, x: "37%", delay: 1 });
+gsap.to(doorOne, { duration: 1.3, y: "-85%" });
+gsap.to(doorTwo, { duration: 1.3, x: "-59%" });
+gsap.to(doorThree, { duration: 1.3, x: "43%" });
 
 gsap.to(doorOne, { duration: 1, opacity: 0.5, delay: 2 });
+
+let cursor = gsap.to(".cursor", {
+  opacity: 0,
+  ease: "power2.inOut",
+  repeat: -1,
+});
+let masterTl = gsap.timeline({ repeat: -1 }).delay(4);
+let boxTl = gsap.timeline();
+
+// .to(".box", { duration: 2, width: "17vw", ease: "power4.inOut" })
+// .from(".hi", { duration: 1, y: "7vw", ease: "power3.out" })
+// .to(".box", {
+//   duration: 3,
+//   height: "7vw",
+//   ease: "elastic.out",
+// })
+// .to(".box", {
+//   duration: 2,
+//   autoAlpha: 0.7,
+//   yoyo: true,
+//   repeat: -1,
+//   ease: "rough({ template: none.out, strength:  1, points: 20, taper: 'none', randomize: true, clamp: false})",
+// });
+words.forEach((word) => {
+  let tl = gsap.timeline({ repeat: 1, yoyo: true, repeatDelay: 1 });
+
+  tl.to(".changing-text", { duration: 2, text: word }, "+=0.5");
+  masterTl.add(tl);
+  masterTl.addPause(0);
+});
+
+boxTl
+
+  .from(".title", {
+    duration: 2,
+    y: -600,
+    delay: 1.1,
+    ease: "power2.inOut",
+  })
+  .from(
+    ".next-btn",
+    {
+      duration: 1,
+      opacity: 0,
+    },
+    "+=5"
+  )
+  .to(".fas", {
+    duration: 0.5,
+    y: "25px",
+    repeat: 5,
+    yoyo: true,
+    ease: new SlowMo(0.1, 0.5),
+  });
+
+//Click events
+nextBtn.addEventListener("click", () => {
+  masterTl
+    .from(".subtitle1", { duration: 3, x: -1200 })
+    .from(".subtitle2", { duration: 3, x: 1400 })
+    .from(".subtitle3", { duration: 3, y: 1200 });
+});
+blueBtn.addEventListener("mouseenter", () => console.log("mouse"));
--- components/UserCard.js
+++ components/UserCard.js
@@ -4,91 +4,10 @@ template.innerHTML = `
 <style>
 
 
-
-  .choose-button1 {
-    position: absolute;
-    right: 1rem;
-    bottom: 1rem;
-    z-index: 70;
-    padding: 15px 30px;
-    border: 2px solid var(--button-color1);
-    margin: 4rem auto;
-
-    text-transform: uppercase;
-    font-weight: 600;
-    letter-spacing: 2px;
-    color: #fff;
-    -webkit-box-reflect: below 0px linear-gradient(transparent, #0002);
-    transition: 0.5s;
-    text-decoration: none;
-
-    transform: rotate(35deg) skew(-24deg)
-  }
-
-  .choose-button1:hover {
-    color: black;
-    box-shadow: 0 0 10px var(--button-color1), 0 0 20px var(--button-color1), 0 0 40px var(--button-color1),
-      0 0 80px var(--button-color1), 0 0 160px var(--button-color1);
-    transition-delay: 1.3s;
-  }
-  .choose-button1 span {
-    position: relative;
-    z-index: 100;
-  }
-  // a:nth-child(2) {
-  //   filter: hue-rotate(240deg);
-  // }
-  .choose-button1:before {
-    content: "";
-    position: absolute;
-    left: -20px;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 20px;
-    height: 2px;
-    background: var(--button-color1);
-    box-shadow: 5px -8px 0 var(--button-color1), 5px 8px 0 var(--button-color1);
-    transition: width 0.5s, left 0.5s, height 0.5s, box-shadow 0.5s;
-    transition-delay: 0.3s, 0.3s, 0s, 0s;
-  }
-  .choose-button1:hover::before {
-    width: 60%;
-    height: 100%;
-    left: -2px;
-    box-shadow: 5px 0 0 var(--button-color1), 5px 0 0 var(--button-color1);
-    transition-delay: 0s, 0.3s, 1s, 0.5s;
-  }
-
-  .choose-button1:after {
-    content: "";
-    position: absolute;
-    right: -20px;
-    top: 50%;
-    transform: translateY(-50%);
-    width: 20px;
-    height: 2px;
-    background: var(--button-color1);
-    box-shadow: -5px -8px 0 var(--button-color1), -5px 8px 0 var(--button-color1);
-    transition: width 0.5s, left 0.5s, height 0.5s, box-shadow 0.5s;
-    transition-delay: 0.3s, 0.3s, 0s, 0s;
-  }
-  .choose-button1:hover::after {
-    width: 60%;
-    height: 100%;
-    right: -2px;
-    box-shadow: -5px 0 0 var(--button-color1), -5px 0 0 var(--button-color1);
-    transition-delay: 0s, 0.3s, 1s, 0.5s;
-  }
-
-
-
-
-
-
    .choose-button {
     position: absolute;
-    left: 1rem;
-    bottom: 1rem;
+    left: 2rem;
+    bottom: 2rem;
     z-index: 70;
 
     padding: 15px 30px;
@@ -102,15 +21,22 @@ template.innerHTML = `
     -webkit-box-reflect: below 0px linear-gradient(transparent, #0002);
     transition: 0.5s;
     text-decoration: none;
+    transform: perspective(400px) rotateY(25deg) rotateZ(-20deg);
+    box-shadow: -20px 20px 10px rgba(66, 108, 223, 0.5);
 
-    transform: rotate(-35deg) skew(24deg)
+  }
+
+  .choose-button:active {
+  box-shadow: 0 5px #666;
+  transform: translateZ(5rem);
+
   }
 
   .choose-button:hover {
     color: black;
     box-shadow: 0 0 10px var(--button-color), 0 0 20px var(--button-color), 0 0 40px var(--button-color),
       0 0 80px var(--button-color), 0 0 160px var(--button-color);
-    transition-delay: 1.3s;
+    transition-delay: 1s;
   }
   .choose-button span {
     position: relative;
@@ -129,8 +55,8 @@ template.innerHTML = `
     height: 2px;
     background: var(--button-color);
     box-shadow: 5px -8px 0 var(--button-color), 5px 8px 0 var(--button-color);
-    transition: width 0.5s, left 0.5s, height 0.5s, box-shadow 0.5s;
-    transition-delay: 0.3s, 0.3s, 0s, 0s;
+    transition: width 0.3s, left 0.5s, height 0.5s, box-shadow 0.5s;
+    transition-delay: 0.1s, 0.1s, 0s, 0s;
   }
   .choose-button:hover::before {
     width: 60%;
@@ -150,8 +76,8 @@ template.innerHTML = `
     height: 2px;
     background: var(--button-color);
     box-shadow: -5px -8px 0 var(--button-color), -5px 8px 0 var(--button-color);
-    transition: width 0.5s, left 0.5s, height 0.5s, box-shadow 0.5s;
-    transition-delay: 0.3s, 0.3s, 0s, 0s;
+    transition: width 0.3s, left 0.5s, height 0.5s, box-shadow 0.5s;
+    transition-delay: 0.1s, 0.1s, 0s, 0s;
   }
   .choose-button:hover::after {
     width: 60%;
@@ -160,10 +86,95 @@ template.innerHTML = `
     box-shadow: -5px 0 0 var(--button-color), -5px 0 0 var(--button-color);
     transition-delay: 0s, 0.3s, 1s, 0.5s;
   }
-</style>
 
-  <a href="#"><span><slot /></span></a>
-`;
+
+  
+  .choose-button1 {
+    position: absolute;
+    right: 2rem;
+    bottom: 2rem;
+    z-index: 70;
+    padding: 15px 30px;
+    border: 2px solid var(--button-color1);
+    margin: 4rem auto;
+
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: 2px;
+    color: #fff;
+    -webkit-box-reflect: below 0px linear-gradient(transparent, #0002);
+    transition: 0.5s;
+    text-decoration: none;
+    transform: perspective(400px) rotateY(-25deg) rotateZ(20deg);
+    box-shadow: 20px 20px 10px rgba(250, 102, 102, 0.5);
+
+  }
+
+  .choose-button1:hover {
+    color: black;
+    box-shadow: 0 0 10px var(--button-color1), 0 0 20px var(--button-color1), 0 0 40px var(--button-color1),
+      0 0 80px var(--button-color1), 0 0 160px var(--button-color1);
+    transition-delay: 1s;
+  }
+  .choose-button1 span {
+    position: relative;
+    z-index: 100;
+  }
+  // a:nth-child(2) {
+  //   filter: hue-rotate(240deg);
+  // }
+  .choose-button1:before {
+    content: "";
+    position: absolute;
+    left: -20px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    height: 2px;
+    background: var(--button-color1);
+    box-shadow: 5px -8px 0 var(--button-color1), 5px 8px 0 var(--button-color1);
+    transition: width 0.3s, left 0.5s, height 0.5s, box-shadow 0.5s;
+    transition-delay: 0.1s, 0.3s, 0s, 0s;
+  }
+  .choose-button1:hover::before {
+    width: 60%;
+    height: 100%;
+    left: -2px;
+    box-shadow: 5px 0 0 var(--button-color1), 5px 0 0 var(--button-color1);
+    transition-delay: 0s, 0.3s, 1s, 0.5s;
+  }
+
+  .choose-button1:after {
+    content: "";
+    position: absolute;
+    right: -20px;
+    top: 50%;
+    transform: translateY(-50%);
+    width: 20px;
+    height: 2px;
+    background: var(--button-color1);
+    box-shadow: -5px -8px 0 var(--button-color1), -5px 8px 0 var(--button-color1);
+    transition: width 0.3s, left 0.5s, height 0.5s, box-shadow 0.5s;
+    transition-delay: 0.1s, 0.3s, 0s, 0s;
+  }
+  .choose-button1:hover::after {
+    width: 60%;
+    height: 100%;
+    right: -2px;
+    box-shadow: -5px 0 0 var(--button-color1), -5px 0 0 var(--button-color1);
+    transition-delay: 0s, 0.3s, 1s, 0.5s;
+  }
+
+  
+
+
+</style>
+<div class="push">
+  <a href="#">
+  <span><slot /></span></a>
+  
+</div>
+  `;
 
 class UserCard extends HTMLElement {
   constructor() {
